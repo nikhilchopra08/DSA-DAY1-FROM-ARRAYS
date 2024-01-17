2a979b8b8c52e965fba38fab2f216d5271e86bdf
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2024-01-17 19:00:41 +0530

    LinearSearch-1

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+bool Search(int arr[] , int n , int key){
+    for (int i = 0; i < n;i++){
+        if(arr[i] == key){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(){
-    
+    int num[5] = {2,4,54,2,-32};
+
+    //Whether number exist or not in array
+    cout << "enter the number that we have to search" << endl;
+    int key;
+    cin >> key;
+
+    bool exist = Search(num , 5 , key);
+
+    if(exist){
+        cout << "Exist"<< endl;
+    }
+    else{
+        cout << "not exist";
+    }
 }
